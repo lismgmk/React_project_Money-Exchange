c3eee37bd74502914cc83beb77273afdeac0254b
lismgmk <lismgmk2@gmail.com>
2021-02-13 22:57:25 +0300

    First commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,47 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+import Header from './Header/Header'
+import Footer from './Footer/Footer'
+import Rate from './Rate/Rate'
+import About from './About/About'
+
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+
+class App extends React.Component {
+  // constructor(props){
+  //   super(props);
+  // }
+  render(){
+      return (
+      <div className="App">
+        <Header />
+          <div className="container">
+            <main>
+              <Router>
+                <Switch>
+                  <Route exact path="/" component = {Rate} />
+                  <Route exact path="/about" component = {About} />
+                </Switch>
+              </Router>               
+            </main>
+      </div>
+
+      <div className="container" id="cookie_info">
+          <div className="site-content">
+              <div className="well">На нашем сайте мы используем cookie для сбора информации технического характера.<br/>В
+                  частности, для персонифицированной работы сайта мы обрабатываем IP-адрес региона вашего
+                  местоположения.&nbsp;
+                  <button className="btn btn-primary btn-sm">OK</button>
+                  </div>
+          </div>
+      </div>
+
+      <Footer />
     </div>
-  );
+    );
+  }
 }
+  
 
 export default App;
